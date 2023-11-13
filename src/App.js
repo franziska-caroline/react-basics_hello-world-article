ed2d6d69ce995475c5cff82a9e4680a742a5202f
Franziska Adam <franziskacarolineadam@gmail.com>
2023-11-13 11:33:43 +0100

    Complete task

--- src/App.js
+++ src/App.js
@@ -2,5 +2,14 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <div>Replace me with your Component!</div>;
+  return HelloWorldArticle();
+}
+
+function HelloWorldArticle() {
+  return (
+    <article>
+      <h1>Hello World!</h1>
+      <p>Wie geht es dir?</p>
+    </article>
+  );
 }
